98f599ebb2c1a0b468c9bcaf0f985651146359e1
Amin Jash <87685883+Aminjash32@users.noreply.github.com>
2024-06-01 11:33:41 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('hamburger-icon').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.add('show');
-});
-
-document.getElementById('close-icon').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.remove('show');
-});
